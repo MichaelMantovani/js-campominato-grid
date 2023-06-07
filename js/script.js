813bff9b1924a607f93aea2e9eacdf8229846e0e
MichaelMantovani <michmantovani@gmail.com>
2023-06-07 15:49:52 +0200

    Collect element from DOM

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 console.log('JS OK')
+
+
+// Recupero gli elementi dal DOM
+const difficultyInput = document.getElementById('difficulty');
+const playButton = document.getElementById('btn-play');
+const grid = document.getElementById('grid')
+
+
+
